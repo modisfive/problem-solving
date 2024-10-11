3e9910c65c4a75da6aa40fd643a9b534d83297be
Lee, Nam Gon <modisfive899@gmail.com>
2024-10-11 16:40:53 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,8 +1,9 @@
-SELECT food_type, rest_id, rest_name, favorites
-FROM rest_info
-WHERE (food_type, favorites) IN (
-    SELECT food_type, MAX(favorites)
+WITH cte AS (
+	SELECT food_type, rest_id, rest_name, favorites, DENSE_RANK() OVER(PARTITION BY food_type ORDER BY favorites DESC) rnk
     FROM rest_info
-    GROUP BY food_type
 )
+
+SELECT food_type, rest_id, rest_name, favorites
+FROM cte
+WHERE rnk = 1
 ORDER BY food_type DESC;
