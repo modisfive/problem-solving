c98a809a42fa94cd1bce355b0dcedcdcb865d20a
Lee, Nam Gon <modisfive899@gmail.com>
2024-10-13 19:24:50 +0900

    [level 2] Title: 부모의 형질을 모두 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
+++ 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -1,4 +1,4 @@
-SELECT a.id id, a.genotype genotype, b.genotype parent_genotype
-FROM ecoli_data a JOIN ecoli_data b ON a.parent_id = b.id
-WHERE a.genotype & b.genotype = b.genotype
-ORDER BY id;
+SELECT child.id, child.genotype genotype, parent.genotype parent_genotype
+FROM ecoli_data child JOIN ecoli_data parent ON child.parent_id = parent.id
+WHERE child.genotype & parent.genotype = parent.genotype
+ORDER BY child.id;
