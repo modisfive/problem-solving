27dc4a743bab5f99c98bec34f1e26cf5e4657519
Lee, Nam Gon <modisfive899@gmail.com>
2024-10-13 19:37:47 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT b.id, b.email, b.first_name, b.last_name
+SELECT DISTINCT b.id id, b.email email, b.first_name first_name, b.last_name last_name
 FROM skillcodes a JOIN developers b ON a.code & b.skill_code
 WHERE a.category = 'Front End'
 ORDER BY b.id;
