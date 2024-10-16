44bb1d58066458128fc3d0ace8e5709bc137059b
Lee, Nam Gon <modisfive899@gmail.com>
2024-10-16 16:47:02 +0900

    [level 4] Title: 도둑질, Time: 26.29 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,6 +1,29 @@
 class Solution {
     public int solution(int[] money) {
         int n = money.length;
+        int[][] dp = new int[2][n];
+        
+        dp[0][0] = money[0];
+        dp[0][1] = money[0];
+        
+        dp[1][0] = 0;
+        dp[1][1] = money[1];
+        
+        for (int i = 0; i < 2; i++) {
+            for (int j = 2; j < n; j++) {
+                dp[i][j] = Math.max(dp[i][j - 1], dp[i][j - 2] + money[j]);
+            }
+        }
+        
+        
+        int answer = Math.max(dp[0][n - 2], dp[1][n - 1]);
+        
+        return answer;
+        
+        
+        
+        
+        /**
         int[] dpO = new int[n]; // 첫번째 집 포함 O
         int[] dpX = new int[n]; // 첫번째 집 포함 X
         
@@ -16,6 +39,8 @@ class Solution {
         }
         
         int answer = Math.max(dpO[n - 2], dpX[n - 1]);
+        
         return answer;
+        */
     }
 }
