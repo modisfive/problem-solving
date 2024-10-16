ad208dfb98df8b035c7b975bcddd74fadf34a2c6
Lee, Nam Gon <modisfive899@gmail.com>
2024-10-16 09:36:15 +0900

    [Silver I] Title: 싫은데요, Time: 376 ms, Memory: 66620 KB -BaekjoonHub

---
+++ 백준/Silver/25916. 싫은데요/싫은데요.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static StringBuilder sb = new StringBuilder();
+  static int n, m;
+  static int[] numbers;
+
+  public static void main(String[] args) throws IOException {
+    setUp();
+
+    int answer = 0;
+    int left = 0;
+    int right = 0;
+    int curr = 0;
+
+    while (right < n) {
+      if (curr < m) {
+        curr += numbers[right];
+        right++;
+      } else {
+        curr -= numbers[left];
+        left++;
+      }
+
+      if (curr <= m) {
+        answer = Math.max(answer, curr);
+      }
+
+    }
+
+    sb.append(answer);
+    output();
+  }
+
+  private static void setUp() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+    numbers = new int[n];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      numbers[i] = Integer.parseInt(st.nextToken());
+    }
+  }
+
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
+}
