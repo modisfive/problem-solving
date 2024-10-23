166aa188e9d9f39e7c252b0d828bef883e22d0b5
Lee, Nam Gon <modisfive899@gmail.com>
2024-10-23 09:13:59 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,45 +11,58 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int n, m;
-	static Map<Integer, List<Integer>> graph;
-	static boolean[] visited;
+  static StringBuilder sb = new StringBuilder();
+  static int n, m, answer;
+  static Map<Integer, List<Integer>> graph;
+  static boolean[] visited;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		n = Integer.parseInt(br.readLine());
-		m = Integer.parseInt(br.readLine());
-		graph = new HashMap<>();
-		for (int i = 1; i < n + 1; i++) {
-			graph.put(i, new ArrayList<>());
-		}
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			graph.get(a).add(b);
-			graph.get(b).add(a);
-		}
+  public static void main(String[] args) throws IOException {
+    setUp();
 
-		visited = new boolean[n + 1];
-		visited[1] = true;
-		int answer = dfs(1);
+    answer = 0;
+    dfs(1);
+    answer--;
 
-		System.out.println(answer);
+    sb.append(answer);
+    output();
+  }
 
-	}
+  private static void dfs(int curr) {
+    visited[curr] = true;
+    answer++;
 
-	private static int dfs(int curr) {
-		int cnt = 0;
-		List<Integer> childs = graph.get(curr);
-		for (Integer child : childs) {
-			if (!visited[child]) {
-				visited[child] = true;
-				cnt += dfs(child) + 1;
-			}
-		}
-		return cnt;
-	}
+    for (int next : graph.get(curr)) {
+      if (!visited[next]) {
+        dfs(next);
+      }
+    }
+  }
+
+  private static void setUp() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    n = Integer.parseInt(br.readLine());
+    m = Integer.parseInt(br.readLine());
+    graph = new HashMap<>();
+    visited = new boolean[n + 1];
+    for (int i = 1; i < n + 1; i++) {
+      graph.put(i, new ArrayList<>());
+    }
+    for (int i = 0; i < m; i++) {
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      graph.get(a).add(b);
+      graph.get(b).add(a);
+    }
+  }
+
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
 
 }
